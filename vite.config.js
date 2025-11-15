b3e7c4bb993493cd20ef7adb6ef3cf5fbb88d6ef
Sarvesh Soni <shubhamsoni1231@rediffmail.com>
2025-11-16 01:22:11 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-    host:true,
-    proxy:'http://localhost:/8000',
-    port:5174
-  },
   plugins: [
     tailwindcss(),
     react()],
